368d0e50df98d715c03e8db8ac87056a27555e92
老腊肉 <ASUS@DESKTOP-OIJIP7L>
2017-11-17 20:17:10 +0800

    xxxx

---
+++ db/initdata.sql
@@ -0,0 +1,6 @@
+
+-- ----------------------------
+-- Records of ada_site
+-- ----------------------------
+INSERT INTO `ada_site` VALUES ('1000', '1', '测试站点', now());
+INSERT INTO `ada_user` VALUES ('10000', 'admin', 'E19D5CD5AF0378DA05F63F891C7467AF', '管理员', '23423', null, null, null, null, null, null, null, null, null, null, '1', now());
