08b0e5eb75e0b8b8fbf62629f24729ad967219d3
老腊肉 <ASUS@DESKTOP-OIJIP7L>
2017-11-22 11:28:07 +0800

    xxx

--- src/main/java/com/ada/app/service/impl/StatServiceImpl.java
+++ src/main/java/com/ada/app/service/impl/StatServiceImpl.java
@@ -3,7 +3,6 @@ package com.ada.app.service.impl;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,7 +18,6 @@ import com.ada.app.service.JedisPools;
 import com.ada.app.service.StatService;
 
 @Service
-@SuppressWarnings("all")
 public class StatServiceImpl implements StatService{
 
 	@Autowired
@@ -27,12 +25,17 @@ public class StatServiceImpl implements StatService{
 
 	/*测试*/
 	public static void main(String[] args) throws Exception {
-//		JedisPoolConfig config = new JedisPoolConfig();
-//		
-//		JedisPool jedisPool = new JedisPool(config, "127.0.0.1", 6379, 20000, "g^h*123T", 2);
-//		StatServiceImpl statServiceImpl = new StatServiceImpl();
+		JedisPoolConfig config = new JedisPoolConfig();
 		
-
+		JedisPool jedisPool = new JedisPool(config, "127.0.0.1", 6379, 20000, "g^h*123T", 2);
+		StatServiceImpl statServiceImpl = new StatServiceImpl();
+//		statServiceImpl.setJedisPool(jedisPool);
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+		Date dt = df.parse("2017-11-15");
+		
+		SiteStat statChannel = statServiceImpl.statSite(2, dt);
+//		ChannelStat channelStat = statServiceImpl.statChannel(2, 2, dt);
+		System.out.println("channelPV:  ==> "+statChannel.getPv()+"channelIP:  ==> "+statChannel.getIp());
 		
 	}
 
@@ -44,12 +47,15 @@ public class StatServiceImpl implements StatService{
 		if(_SitePV != null) sitePV = Integer.valueOf(_SitePV);
 		//取出站点IPSet集合
 		int siteIP = jedis.scard("SiteIP_"+site+"").intValue();
+		
+		returnRes(jedis);
+		
 		return new SiteStat(site, siteIP, sitePV, date);
 	}
 	
 	public ChannelStat statChannel(Integer siteId, Integer channelId, Date date) {
 		Jedis jedis = getJedis(date);
-		Integer site_ChannelPV = 0;
+		Integer sitePV = 0;
 		Integer clickip1 = 0;
 		Integer clickip2 = 0;
 		Integer clickip3 = 0;
@@ -59,7 +65,7 @@ public class StatServiceImpl implements StatService{
 		int channelIP = jedis.scard("ChannelIP_"+channelId+"").intValue();
 		//取出渠道PV 
 		String ChannelPV = jedis.get("ChannelPV_"+channelId+"");
-		if(ChannelPV != null) site_ChannelPV = Integer.valueOf(ChannelPV);
+		if(ChannelPV != null) sitePV = Integer.valueOf(ChannelPV);
 		//取出渠道进入目标页IP集合
 		int targetpageIP  = jedis.scard("ChannelTIP_"+channelId+"").intValue();
 		//取出渠道多个点击区间次数
@@ -75,45 +81,24 @@ public class StatServiceImpl implements StatService{
 		String ChannelC4IP = jedis.get("ChannelC4IP_"+channelId+"");
 		if(ChannelC4IP != null) clickip4 = Integer.valueOf(ChannelC4IP);
 		
-		return new ChannelStat(siteId, channelId, channelIP, site_ChannelPV, clickip1, clickip2, clickip3, clickip4, targetpageIP, date);
+		returnRes(jedis);
+		
+		return new ChannelStat(siteId, channelId, channelIP, sitePV, clickip1, clickip2, clickip3, clickip4, targetpageIP, date);
 	}
 	
+	@Override
 	public DomainStat statDomain(Integer siteId, Integer domainId, Date date) {
-		Jedis jedis = getJedis(date);
-		Integer site_domainPV = 0;
-		Integer clickip1 = 0;
-		Integer clickip2 = 0;
-		Integer clickip3 = 0;
-		Integer clickip4 = 0;
-		
-		//取出域名IPSet集合
-		int domainIP = jedis.scard("DomainIP_"+domainId+"").intValue();
-		//取出域名PV 
-		String domainPV = jedis.get("DomainPV_"+domainId+"");
-		if(domainPV != null) site_domainPV = Integer.valueOf(domainPV);
-		//取出域名进入目标页IP集合
-		int targetpageIP  = jedis.scard("DomainTIP_"+domainId+"").intValue();
-		//取出域名多个点击区间次数
-		String domainC1IP = jedis.get("DomainC1IP_"+domainId+"");
-		if(domainC1IP != null) clickip1 = Integer.valueOf(domainC1IP);
-		
-		String domainC2IP = jedis.get("DomainC2IP_"+domainId+"");
-		if(domainC2IP != null) clickip2 = Integer.valueOf(domainC2IP);
-		
-		String domainC3IP = jedis.get("DomainC3IP_"+domainId+"");
-		if(domainC3IP != null) clickip3 = Integer.valueOf(domainC3IP);
-		
-		String domainC4IP = jedis.get("DomainC4IP_"+domainId+"");
-		if(domainC4IP != null) clickip4 = Integer.valueOf(domainC4IP);
-		
-		return new DomainStat(siteId, domainId, domainIP, site_domainPV, clickip1, clickip2, clickip3, clickip4, targetpageIP, date);
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	protected Jedis getJedis(Date date){	
 		return jedisPools.getResource(date.getDay());
 	}
 	
-	public void returnResource(Date date,Jedis jedis) {
-		jedisPools.returnResource(date.getDay(),jedis);
+	protected void returnRes(Jedis jedis) {
+		jedisPools.returnResource(jedis);
     }
+
+
 }
