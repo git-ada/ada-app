1b1c4f9bb5dba6aaead2509a7b7ef17d2240a4f2
咸菜 <ASUS@DESKTOP-UTSB1A5>
2017-12-11 20:55:50 +0800

    zx：修改站点ID为“undefined”BUG

--- src/main/webapp/log.js
+++ src/main/webapp/log.js
@@ -1,6 +1,5 @@
 /** 日志服务器地址 **/
-//var adaLogServer = "http://45.125.219.106:8001";
-var adaLogServer = "http://127.0.0.1:8082";
+var adaLogServer = "http://45.125.219.106:8001";
 var adaPageInTime = 0;
 var adaSiteId;
 var adaClientId;
@@ -24,9 +23,7 @@ function adaPageIn(){
 
 		/** 获得站点ID **/
 		adaSiteId = adaGetSiteId();
-		if(adaSiteId == "undefined"){
-			adaSiteId = "";
-		}
+		if(adaSiteId == "undefined") adaSiteId = "";
 		
 		/** 获取广告ID **/
 		if(adaIsCookieExists("bgn59Wyo")){
