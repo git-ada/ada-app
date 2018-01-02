f042ca655f56ac824fb9136952c74e266c0fe31a
咸菜 <ASUS@DESKTOP-UTSB1A5>
2018-01-02 10:39:59 +0800

    添加分时统计返回时的参数

--- src/main/webapp/assets/js/dashboard_zx.js
+++ src/main/webapp/assets/js/dashboard_zx.js
@@ -783,7 +783,7 @@ function graphicLoading(obj) {
 //打开菜单
 function openMenu(a,event){
 	
-	var backUrl = webPath+"/dashboardHistry.jhtm?dataType="+dataType+"&clickDate="+clickDate;
+	var backUrl = webPath+"/dashboardHistry.jhtm?dataType="+dataType+"&clickDate="+clickDate+"&top="+ipTop+"&doamin="+search;
 	var id = jQuery(a).attr("id");
 	var domain = jQuery(a).attr("domain");
 
